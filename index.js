9357adf1b320a8d85e603da27148cb9e9a7b3012
ZulfahmiIF06 <149761239+ZulfahmiIF06@users.noreply.github.com>
2025-07-29 00:42:41 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,313 @@
+import PaketImage1 from "../assets/img/rajaampat.jpeg";
+import PaketImage2 from "../assets/img/img1.jpg";
+import PaketImage3 from "../assets/img/Benteng Kesultanan Buton.jpeg";
+import PaketImage4 from "../assets/img/hugo-matilla-EFuCATqfblI-unsplash.jpg";
+import PaketImage5 from "../assets/img/fikri-rasyid-IabAwWbXr1Y-unsplash.jpg";
+import PaketImage6 from "../assets/img/dapiki-moto-RAL3lZU3j0E-unsplash.jpg";
+import PaketImage7 from "../assets/img/borobudur.jpeg";
+import PaketImage8 from "../assets/img/monas.jpeg";
+import PaketImage9 from "../assets/img/bali.jpeg";
+import PaketImage10 from "../assets/img/fstvl 1.jpeg";
+import PaketImage11 from "../assets/img/fstl 2.jpeg";
+import PaketImage12 from "../assets/img/fstvl 3.jpeg";
+import PaketImage13 from "../assets/img/fstvl 4.jpeg";
+import PaketImage14 from "../assets/img/fstvl 5.jpeg";
+import PaketImage15 from "../assets/img/fstvl 6.jpeg";
+import PaketImage16 from "../assets/img/fstvl 7.jpeg";
+import PaketImage17 from "../assets/img/fstvl 8.jpeg";
+
+export const navLinks = [
+  {
+    id: 1,
+    path: "",
+    text: "Home",
+  },
+  {
+    id: 2,
+    path: "Destination",
+    text: "Destination",
+  },
+  {
+    id: 3,
+    path: "Event",
+    text: "Event",
+  },
+  {
+    id: 5,
+    path: "SyaratKet",
+    text: "Syarat & Ketentuan",
+  },
+];
+
+export const Keunggulan = [
+  {
+    id: 1,
+    eventKey: 0,
+    title: "Dukungan untuk Pariwisata Lokal",
+    desc: "Dengan menggunakan Travel Kita, Anda ikut berkontribusi mendukung ekonomi lokal dan pelestarian budaya daerah yang dikunjungi.",
+  },
+  {
+    id: 2,
+    eventKey: 1,
+    title: "Destinasi Yang Menarik",
+    desc: "Travel Kita menawarkan beragam pilihan destinasi wisata terbaik di seluruh Indonesia, dari wisata alam, budaya, kuliner, hingga spot viral kekinian.",
+  },
+  {
+    id: 3,
+    eventKey: 2,
+    title: "Pemandu Wisata Profesional",
+    desc: "Didampingi oleh pemandu lokal yang berpengalaman dan ramah, perjalanan Anda akan lebih aman, informatif, dan menyenangkan.",
+  },
+  {
+    id: 4,
+    eventKey: 3,
+    title: "Harga terjamin",
+    desc: "Paket wisata disusun dengan harga bersaing tanpa biaya tersembunyi, cocok untuk solo traveler, keluarga, maupun rombongan.",
+  },
+  {
+    id: 5,
+    eventKey: 4,
+    title: "Pelayanan Yang Cepat dan Mudah",
+    desc: "Tim layanan pelanggan kami siap membantu Anda kapan saja, mulai dari konsultasi perjalanan hingga penanganan saat di lokasi.",
+  },
+  {
+    id: 6,
+    eventKey: 5,
+    title: "Fleksibel & Customizable",
+    desc: "Travel Kita menyediakan layanan paket wisata yang dapat disesuaikan dengan kebutuhan dan preferensi Anda—lebih personal dan nyaman.",
+  },
+];
+
+export const semuaPaket = [
+  {
+    id: 1,
+    image: PaketImage1,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star-half-stroke",
+    title: "Tour Gunung Bromo",
+    price: "Rp. 1.000.000",
+    buy: "Beli Tiket",
+    delay: "1300",
+  },
+  {
+    id: 2,
+    image: PaketImage2,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Wisata Candi Borobudur",
+    price: "Rp. 340.000",
+    buy: "Beli Kelas",
+    delay: "1600",
+  },
+  {
+    id: 3,
+    image: PaketImage3,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Wisata Gunung Merbabu",
+    price: "Rp. 280.000",
+    buy: "Beli Kelas",
+    delay: "1900",
+  },
+  {
+    id: 4,
+    image: PaketImage4,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Mastering Logo with Adobe Illustrator",
+    price: "Rp. 420.000",
+    buy: "Beli Kelas",
+    delay: "",
+  },
+  {
+    id: 5,
+    image: PaketImage5,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "3D Modelling for Beginner with 3D Blender",
+    price: "Rp. 380.000",
+    buy: "Beli Kelas",
+    delay: "300",
+  },
+  {
+    id: 6,
+    image: PaketImage6,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Mastering Icon Design with Adobe Illustrator",
+    price: "Rp. 300.000",
+    buy: "Beli Kelas",
+    delay: "600",
+  },
+  {
+    id: 7,
+    image: PaketImage7,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star-half-stroke",
+    title: "Design Vektor with Corel Draw CC for Beginner",
+    price: "Rp. 340.000",
+    buy: "Beli Kelas",
+    delay: "",
+  },
+  {
+    id: 8,
+    image: PaketImage8,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star-half-stroke",
+    title: "UI UX Design with Adobe XD for Beginner",
+    price: "Rp. 400.000",
+    buy: "Beli Kelas",
+    delay: "300",
+  },
+  {
+    id: 9,
+    image: PaketImage9,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star-half-stroke",
+    title: "Mastering Mobile Developer with Flutter",
+    price: "Rp. 320.000",
+    buy: "Beli Kelas",
+    delay: "600",
+  },
+];
+
+export const PaketTerbaik = [
+  {
+    id: 1,
+    image: PaketImage1,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Paket Wisata Raja Ampat",
+    price: "Rp. 340.000",
+    buy: "Beli Tiket",
+    delay: "200",
+  },
+  {
+    id: 2,
+    image: PaketImage2,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Wisata pendakian dengan ketenangan",
+    price: "Rp. 340.000",
+    buy: "Beli Tiket",
+    delay: "300",
+  },
+  {
+    id: 3,
+    image: PaketImage3,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Wisata Sejarah Kesultanan Buton",
+    price: "Rp. 780.000",
+    buy: "Beli Tiket",
+    delay: "400",
+  },
+  {
+    id: 4,
+    image: PaketImage4,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Mastering Logo with Adobe Illustrator",
+    price: "Rp. 420.000",
+    buy: "Beli Kelas",
+    delay: "",
+  },
+  {
+    id: 5,
+    image: PaketImage5,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "3D Modelling for Beginner with 3D Blender",
+    price: "Rp. 380.000",
+    buy: "Beli Kelas",
+    delay: "200",
+  },
+  {
+    id: 6,
+    image: PaketImage6,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Mastering Icon Design with Adobe Illustrator",
+    price: "Rp. 300.000",
+    buy: "Beli Kelas",
+    delay: "400",
+  },
+];
+
+export const EventPage = [
+  {
+    id: 1,
+    image: PaketImage10,
+    title: "Fetival budaya bali",
+    paragraf:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    delay: "1300",
+  },
+
+  {
+    id: 2,
+    image: PaketImage11,
+    title: "Festival Budaya Jawa",
+    paragraf: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    delay: "1600",
+  },
+  {
+    id: 3,
+    image: PaketImage12,
+    title: "Kunjungan Presiden Ke Wilayah Buton",
+    paragraf: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    delay: "1900",
+  },
+  {
+    id: 4,
+    image: PaketImage13,
+    title: "Festival Lomaba Tarian Daerah Aceh",
+    paragraf: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    delay: "1900",
+  },
+
+];
